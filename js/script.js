1406045052c39faa6bccb4220c6c697b4984d1e2
GlaIZier <kh.mv@bk.ru>
2014-03-29 19:12:31 +0400

    New animations was added. Bugs were fixed. Markup and styles were corrected.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 function $(id) { return document.getElementById(id); }
 
 // get elements by tag name inside parent element or inseide document if it is undefined
-function $$(tagName, parentElement) { 
-   parentElement = parentElement || document; 
+function $$(tagName, parentElement) {
+   if (typeof (parentElement) === "undefined") parentElement = document;
    return parentElement.getElementsByTagName(tagName); 
 }
 
@@ -21,16 +21,20 @@ var Gallery = {};
 
 /**
  * Does preparatory actions before Gallery usage
- */
+*/
 Gallery.init = function() {
    Gallery._THUMBS_PORTION = 10;
+   Gallery._PHOTOVIDEO_RATIO = parseInt(getStyle($("fullsize"), "width"), 10) /
+      parseInt(getStyle($("fullsize"), "height"), 10);
+   Gallery._PX_TO_SLIDE = parseInt(getStyle($("slidearea"), "width"), 10);
    Gallery._SLIDER_SPACE = 4;
-   Gallery._PX_TO_SLIDE = parseInt(getStyle($('slidearea'), 'width'), 10);
+   Gallery._PX_TO_CLIMB_UP = 71;
    // animation 
-   Gallery._ANIMATION_FRAME_RATE = 50;
+   Gallery._ANIMATION_FRAME_RATE = 80;
    // speed per frame
    Gallery._PX_SLIDE_SPEED = 15;
-   Gallery._OPACITY_SPEED = 0.2;
+   Gallery._OPACITY_SPEED = 0.08;
+   Gallery._PX_CLIMB_UP_SPEED = Gallery._OPACITY_SPEED * 100;
    
    // array for next to load thumb resource, servlet link and thumbnails 
    Gallery.photoVideoHolder = Gallery.formPhotoVideoList();
@@ -39,62 +43,80 @@ Gallery.init = function() {
    Gallery.loadShownPhotoVideo();
    Gallery.loadThumbsPortion();
    
-// TODO delete this later
-//   Gallery._slideLeftElement = $('slideleft');
-//   Gallery._slideRightElement = $('slideright');
-// $('slideleft').onclick = Gallery._slideLeft;
-// $('slideright').onclick = Gallery._slideRight;
-   
    // add next-prev events 
    $("next").onclick = function() { if (Gallery.shownPhotoVideoNum === Gallery.photoVideoHolder.length - 1) return; 
       Gallery.shownPhotoVideoNum++; Gallery.loadShownPhotoVideo(); };
    $("prev").onclick = function() { if (Gallery.shownPhotoVideoNum === 1) return; 
       Gallery.shownPhotoVideoNum--; Gallery.loadShownPhotoVideo(); };
    // add sliders events in thumbs
-   $('slideleft').onclick = function() { Gallery._animateSlide(-1); };
-   $('slideright').onclick = function() { Gallery._animateSlide(1); };
-   
-   Gallery.fillSliderWidth();
+   $("slideleft").onclick = function() { Gallery._animateSlide(-1); };
+   $("slideright").onclick = function() { Gallery._animateSlide(1); };
 };
 
 /**
  * Returns 2d array with photo-video resources:
- * 0                                               1              2  ... 
- * index of next resource to add in slider   resource-link  resource-link
- * servlet link                                img/video      img/video 
+ * 0                                               1                2    ...
+ * index of next resource to add in slider   <resource-link>  <resource-link>
+ * servlet link                                img/video         img/video
+ *                                           <description>     <description>
+ * Example input resources string: "1.jpg:img:blah-blah-blah-blah; http://ya.ru/2.mp4:video"
  */
 Gallery.formPhotoVideoList = function() {
    var photoVideoHolder = [];
    // form service info
-   photoVideoHolder[0] = [1, $('servletlink').value];
+   photoVideoHolder[0] = [1, $("servletlink").value];
    // split to array using regexp. token ';' with >=0 space symbols from both sides 
-   var resources = $('photovideolist').value.split(/\s*;\s*/);
+   var resources = $("photovideolist").value.split(/\s*;\s*/);
    // add resources to photoVideoHolder in format specified above
    for (var resourceNum = 0; resourceNum < resources.length; resourceNum++) {
       var resourceElement = resources[resourceNum].split(/\s*:\s*/);
+      // if there is no description then description = name of file
+      if (typeof (resourceElement[2]) === "undefined" || (resourceElement[2] === "")) {
+         var fileName = resourceElement[0].split(/\s*\.\s*/);
+         resourceElement[2] = fileName[0];
+      }
       photoVideoHolder[resourceNum + 1] = resourceElement;
    }
    return photoVideoHolder;
 };
 
 /**
- * Loads a batch of thumbs to slider in markup
+ * Loads a batch of thumbs to div 'slider' in markup
  */
 Gallery.loadThumbsPortion = function() {
    var appendedMarkup = "";
+   // load a batch or until the end of thumbs
    for (var thumbNum = Gallery.photoVideoHolder[0][0]; 
       (thumbNum < Gallery.photoVideoHolder[0][0] + Gallery._THUMBS_PORTION) &&
       (thumbNum < Gallery.photoVideoHolder.length); thumbNum++) {
       appendedMarkup += Gallery._getMarkupPhotoVideo(thumbNum, Gallery.photoVideoHolder[0][1]);
    }
-   $('slider').insertAdjacentHTML('beforeEnd', appendedMarkup);
+   $("slider").insertAdjacentHTML("beforeEnd", appendedMarkup);
    // add onclick listeners to new thumbs
-   var sliderChildren = $('slider').children;
-   // TODO look for closure
+   var sliderChildren = $("slider").children;
    for (thumbNum = Gallery.photoVideoHolder[0][0] - 1; thumbNum < sliderChildren.length; 
       thumbNum++) {
       (function (_thumbNum) {
-         sliderChildren[thumbNum].addEventListener( 'click', function (e) {
+         // add listeners of loading images and video to calculate width
+         // calc width of the slider several times because we don't know which one will be loaded last
+         if (sliderChildren[_thumbNum].tagName.toLowerCase() === "img") {
+            sliderChildren[_thumbNum].addEventListener("load", function (e) {
+               Gallery.fillSliderWidth();
+            }, false);
+         }
+         else if (sliderChildren[_thumbNum].tagName.toLowerCase() === "video") {
+            sliderChildren[_thumbNum].addEventListener( "loadedmetadata", function (e) {
+               Gallery.fillSliderWidth();
+            }, false);
+         }
+         else {
+            console.error("Unknown tag inside 'slider' div!");
+         }
+         // add click listeners to thumbs
+         // use closure to save current thumbNum from 'for' loop
+         sliderChildren[_thumbNum].addEventListener( "click", function (e) {
+            // if was click on the same picture no animation
+            if (Gallery.shownPhotoVideoNum ===  _thumbNum + 1) return;
             Gallery.shownPhotoVideoNum = _thumbNum + 1;
             Gallery.loadShownPhotoVideo();
          }, false);
@@ -102,39 +124,97 @@ Gallery.loadThumbsPortion = function() {
    }
    // update next first thumb to add
    Gallery.photoVideoHolder[0][0] = thumbNum + 1;
+   // in the case of listener never trigs. photovideo had been loaded earlier than code was executed
+   Gallery.fillSliderWidth();
 };
 
 /**
  * Loads new photo or video to frame
- * Call this function without parameters!
+ * Call this function without parameters (or only with direction parameter).
  */
-//direction: '-1' - fade; '+1' - ignition
-// fade to opacity 0, change photo or video and flare to opacity 1
+// Direction: '-1' - fade; '+1' - ignition
+// Alg: animate info slide down; fade to opacity 0, change photo or video; check image photovideo ratio,
+//    flare to opacity 1 and animate info slide up. (synchronous execution of async functions).
 Gallery.loadShownPhotoVideo = function(direction, opacity) {
    // was called externally
-   if (typeof (direction) === "undefined") direction = -1;
+   if (typeof (direction) === "undefined") {
+      direction = -1;
+      // start info animation down
+      Gallery._animateInfo();
+      return;
+   }
    // fade from 1 (initial) to 0
    if (typeof (opacity) === "undefined" && direction === -1) opacity = 1;
    // ignition from 0 (initial) to 1
    if (typeof (opacity) === "undefined" && direction === 1) opacity = 0;
    
-   // if fade ended change html, do ignition and stop fade (return from this function)
-   if ((opacity < 0 && direction === -1) || (!$("photovideo").children[0])) {
+   // if fade ended (or it was call from init) change html, check ratio, start ignition and stop fade
+   if ((opacity < 0 && direction === -1) || (!$("photovideo").children[$("photovideo").children.length - 1])) {
       $("photovideo").innerHTML = Gallery._getMarkupPhotoVideo(Gallery.shownPhotoVideoNum, "photo-video/");
+      Gallery._repairElementRatio($("photovideo").children[$("photovideo").children.length - 1]);
       Gallery.loadShownPhotoVideo(1);
       return;
    }
    // end of animation (ignition ended)
    if (opacity > 1 && direction === 1) {
+      // start slide up info animation
+      Gallery._animateInfo(1);
       return;
    }
-   
-   $("photovideo").children[0].style.opacity = opacity;
+
+   $("photovideo").children[$("photovideo").children.length - 1].style.opacity = opacity;
    window.setTimeout(function () {
       Gallery.loadShownPhotoVideo(direction, opacity + direction * Gallery._OPACITY_SPEED);
    }, 1000 / Gallery._ANIMATION_FRAME_RATE);
 };
 
+
+// Animates information for Photo Video frame down and then up
+// Call function without parameters or only with direction parameter.
+// Direction: "-1" - animate slide down; "+1" - animate slide up
+Gallery._animateInfo = function(direction, slidedPixels) {
+   // if func has been just called without parameters
+   if (typeof (direction) === "undefined") direction = -1;
+   // if func has been just called to slide down
+   if (typeof (slidedPixels) === "undefined" && direction === -1) {
+      // if call from init or frame was video don't show animation
+      if (!$("information").children[0] ||
+         $("photovideo").children[$("photovideo").children.length - 1].tagName.toLowerCase() === "video") {
+         // start animation to show new frame
+         Gallery.loadShownPhotoVideo(-1);
+         return;
+      }
+      slidedPixels = Gallery._PX_TO_CLIMB_UP;
+   }
+   // if func has been just called to slide up
+   if (typeof (slidedPixels) === "undefined" && direction === 1) {
+      slidedPixels = 0;
+      // add markup inside 'information' div
+      $("information").innerHTML = "<p>" + Gallery.photoVideoHolder[Gallery.shownPhotoVideoNum][2] + "</p>";
+      // if video in frame then don't show information
+      if ($("photovideo").children[$("photovideo").children.length - 1] &&
+         $("photovideo").children[$("photovideo").children.length - 1].tagName.toLowerCase() === "video") return;
+   }
+
+   // stop animation slide down (stop recursion)
+   if (slidedPixels < 0 && direction === -1) {
+      $("information").innerHTML = "";
+      $("information").style.height = "0px";
+      // start animation to show new frame
+      Gallery.loadShownPhotoVideo(-1);
+      return;
+   }
+   // stop animation slide up (stop recursion)
+   if (slidedPixels > Gallery._PX_TO_CLIMB_UP && direction === 1) {
+      $("information").style.height = Gallery._PX_TO_CLIMB_UP + "px";
+      return;
+   }
+
+   $("information").style.height = slidedPixels + "px";
+   window.setTimeout(function () {
+      Gallery._animateInfo(direction, slidedPixels + direction * Gallery._PX_CLIMB_UP_SPEED);
+   }, 1000 / Gallery._ANIMATION_FRAME_RATE);
+};
 
 // Returns string with markup for photo or video tag with source specified in photoVideoHolder[photoVideoIndex]
 Gallery._getMarkupPhotoVideo = function(photoVideoIndex, imgSrcPreamble) {
@@ -150,32 +230,15 @@ Gallery._getMarkupPhotoVideo = function(photoVideoIndex, imgSrcPreamble) {
          "</object> </video> ";
    }
    else {
-      alert("Error in resources types!");
+      console.error("Unknown tags inside photoVideoHolder object!");
       return;
    }
 };
 
 /**
- * Calculate width of the slider and fill it's width 
- */
+   Calculates and fills 'width' style property of thumbnails ('slider' div)
+*/
 Gallery.fillSliderWidth = function() {
-   // in the case when loadedmetadata triggered earlier than this code
-   // call _fillSliderWidth() several times. Not the best solution. More details: 
-   // http://dev.opera.com/articles/view/consistent-event-firing-with-html5-video/
-   Gallery._fillSliderWidth();
-   var videoList = $$("video", $("slider"));
-   // only when all videos are loaded
-   if (videoList[0]) {
-      for (var videoNumber = 0; videoNumber < videoList.length; videoNumber++) {
-         //TODO make closure to load last video
-         videoList[videoNumber].addEventListener( "loadedmetadata", function (e) {
-            Gallery._fillSliderWidth();
-         }, false);
-      }
-   }
-};
-
-Gallery._fillSliderWidth = function() {
    var sliderElement = $("slider");
    var imgList = $$("img", sliderElement);
    var videoList = $$("video", sliderElement);
@@ -195,37 +258,75 @@ Gallery._fillSliderWidth = function() {
    sliderElement.style.width = sliderWidth + "px"; //sliderElement.setAttribute("style","width:" + sliderWidth + "px");
 };
 
-// TODO delete after test
-//Gallery._slideRight = function() {
-//   Gallery._animateSlide(1, 0);
-//};
-//
-//Gallery._slideLeft = function() {
-//   Gallery._animateSlide(-1, 0);
-//};
-
-//direction: "-1" - animate to left; "+1" - animate to right
+// Animate slide in thumbnails
+// Call function only with direction parameter or without at all(direction = -1 then);
+// Direction: "-1" - animate to left; "+1" - animate to right
 Gallery._animateSlide = function(direction, slidedPixels) {
-   slidedPixels = slidedPixels || 0;
+   if (typeof (direction) === "undefined") direction = -1;
+   // if it was called first time
+   if (typeof (slidedPixels) === "undefined") slidedPixels = 0;
    var prevLeft = parseInt(getStyle($("slider"), "left"), 10);
    var slideAreaWidth = parseInt(getStyle($("slidearea"), "width"), 10);
    var sliderWidth = parseInt(getStyle($("slider"), "width"), 10);
-   // if was slided given number of pixels, or stop at right corner, or stop at left corner 
-   if (slidedPixels >= Gallery._PX_TO_SLIDE) return;
+
+   // stop animation if was slided given number of pixels, or stop at right corner, or stop at left corner
+   if (slidedPixels >= Gallery._PX_TO_SLIDE) {
+      Gallery._animateSlide.isAnimation = false;
+      return;
+   }
    // stop at left corner of slider
    if (prevLeft > 0) {
       $("slider").style.left = "0px";
+      Gallery._animateSlide.isAnimation = false;
       return;
    }
    //stop at right corner of slider
    if (-prevLeft > sliderWidth - slideAreaWidth) {
-      $("slider").style.left = -sliderWidth + slideAreaWidth +"px";
+      $("slider").style.left = -sliderWidth + slideAreaWidth + "px";
       Gallery.loadThumbsPortion();
-      Gallery.fillSliderWidth();
+      Gallery._animateSlide.isAnimation = false;
       return;
    }
+
    $("slider").style.left = (prevLeft - direction * Gallery._PX_SLIDE_SPEED) + "px";
    window.setTimeout(function () {
       Gallery._animateSlide(direction, slidedPixels + Gallery._PX_SLIDE_SPEED);
    }, 1000 / Gallery._ANIMATION_FRAME_RATE);
+};
+
+// Repair image ratio if it doesn't fit to the frame horizontal
+Gallery._repairElementRatio = function(element) {
+   if (element.tagName.toLowerCase() === "img") {
+      element.addEventListener("load", function (e) {
+         var ratio = (element.width / element.height);
+         if (ratio >= Gallery._PHOTOVIDEO_RATIO) {
+            // insert help span to make vertical align middle
+            // more info: https://stackoverflow.com/questions/7273338/how-to-vertically-align-an-image-inside-div
+            Gallery._insertHelpSpan($("photovideo"));
+            // correct image styles
+            element.style.height = "auto";
+            element.style.maxHeight = 300 + "px";
+         }
+      }, false);
+   }
+   else if(element.tagName.toLowerCase === "video") {
+      element.addEventListener("load", function (e) {
+         var ratio = (element.getBoundingClientRect().width / element.getBoundingClientRect().height);
+         if (ratio >= Gallery._PHOTOVIDEO_RATIO) {
+            console.warn("Can't repair ratio for video.");
+         }
+      }, false);
+   }
+   else {
+      console.error("Unknown tag inside repair ratio function! Can be only <img> or <video>.");
+   }
+};
+
+// insert help span for Gallery._setElementRatio method
+Gallery._insertHelpSpan = function(insideElement) {
+   var helpSpan = "<span id='helpspan'></span>";
+   insideElement.insertAdjacentHTML("afterBegin", helpSpan);
+   $("helpspan").style.display = "inline-block";
+   $("helpspan").style.height = "100%";
+   $("helpspan").style.verticalAlign = "middle";
 };
